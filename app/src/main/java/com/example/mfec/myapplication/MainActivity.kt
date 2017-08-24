45b46cf8aa4decc510a2e2f088014aa8f1e039f6
Janejiras <janejira271037@gmail.com>
2017-08-24 16:34:28 +0700

    commit

--- app/src/main/java/com/example/mfec/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/mfec/myapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mfec.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        //Commit
     }
 }
